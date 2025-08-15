fb79691be00fb87343e756cbfb22fec99921135e
Eduardo-Castelleti <111083169+Eduardo-Castelleti@users.noreply.github.com>
2025-08-15 13:38:08 -0300

    Create firebase.jsfirebase.js

---
+++ assets/js/firebase.jsfirebase.js
@@ -0,0 +1,26 @@
+// /assets/js/firebase.js
+// Importa módulos do Firebase (usando import dinâmico via CDN)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.6.1/firebase-auth.js";
+
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDVr6OOQRt2rB0kByInjqjdyoQCpnMAt7I",
+  authDomain: "site-f6495.firebaseapp.com",
+  projectId: "site-f6495",
+  storageBucket: "site-f6495.firebasestorage.app",
+  messagingSenderId: "230161402024",
+  appId: "1:230161402024:web:462a911919ebf8a0809a9d",
+  measurementId: "G-51G8XF6JH0"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa Realtime Database e Auth
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+// Exporta para uso em outros scripts
+export { db, auth };
